ef4b0b69abfb339a899196a63ac8408e6aaec047
adiitya343 <141210078+adiitya343@users.noreply.github.com>
2025-01-08 16:46:45 +0530

    degree.py

--- degree.py
+++ degree.py
@@ -1,8 +1,8 @@
 # code for Degree celsius to fahrenheit
 
-cel= float(input("Enter temperature in celsius : ")) # input from user
+celsius= float(input("Enter temperature in celsius : ")) # input from user
 
-fahren = (cel * 1.8) + 32 #formula for converting deg to fahren
+fahrenheit = (celsius * 1.8) + 32 #formula for converting degree celsius to fahrenheit
 
-print(str(cel)+ " degree celsius is equal to " + str(fahren)+" degree fahrenheit")
+print(str(celsius)+ " degree celsius is equal to " + str(fahrenheit)+" degree fahrenheit")
 
