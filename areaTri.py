91a44a190686e04ac184ee95635598fcd1d555e8
adiitya343 <141210078+adiitya343@users.noreply.github.com>
2025-01-08 16:45:17 +0530

    areaTri.py

--- areaTri.py
+++ areaTri.py
@@ -4,13 +4,13 @@
 
 #first we will take input from user for all 3 sides of triangle
 
-s1 = int(input("Enter 1st side triangle length : "))
-s2 = int(input("Enter 2nd side triangle length : "))
-s3 = int(input("Enter 3rd side triangle length : "))
+side1 = int(input("Enter 1st side triangle length : "))
+side2 = int(input("Enter 2nd side triangle length : "))
+side3 = int(input("Enter 3rd side triangle length : "))
 
-def areaTriangle(s1, s2, s3):  #defining fuctions to calculate area of triangle by using semi perimeter 
-    s = (s1+s2+s3)/2
-    return (s*(s-s1)*(s*s2)*(s*s3))**0.5
+def areaTriangle(side1, side2, side3):  #defining fuctions to calculate area of triangle by using semi perimeter 
+    semiPerimeter = (side1 + side2 + side3)/2
+    return (semiPerimeter*(semiPerimeter-side1)*(semiPerimeter*side2)*(semiPerimeter*side3))**0.5
 
-print("Area = {:.2f}".format(areaTriangle(s1, s2, s3))) # calling function and print the area
+print("Area = {:.2f}".format(areaTriangle(side1, side2, side3))) # calling function and print the area
 
