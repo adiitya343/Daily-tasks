9c0776ede0b5ab1aa28f43c4427f7669124bb15f
adiitya343 <141210078+adiitya343@users.noreply.github.com>
2025-01-08 16:57:42 +0530

    Delete degree.py

--- degree.py
+++
@@ -1,8 +0,0 @@
-# code for Degree celsius to fahrenheit
-
-celsius= float(input("Enter temperature in celsius : ")) # input from user
-
-fahrenheit = (celsius * 1.8) + 32 #formula for converting degree celsius to fahrenheit
-
-print(str(celsius)+ " degree celsius is equal to " + str(fahrenheit)+" degree fahrenheit")
-
